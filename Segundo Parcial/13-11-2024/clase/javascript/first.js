5b6042b1c04840ac8d6252127250e9ef9ac795be
MateoJa54 <143151369+MateoJa54@users.noreply.github.com>
2024-12-16 11:05:50 -0500

    segundo Parial

---
+++ Segundo Parcial/13-11-2024/clase/javascript/first.js
@@ -0,0 +1,22 @@
+var nombre = "Mateo";
+var apellido = "Iza";
+var edad= 20;
+console.log("Mi nombre es "+ nombre +" "+apellido);
+console.log("Mi edad es "+ edad);
+console.log("Mi nombre es "+nombre+" " + apellido + " y tengo " + edad + " años")
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z);
+
+//var declara una variable global
+//let declara una variablee local o tambien una variable de bloqueç
+//conts declara una variable que no va a cambiar su valor 
+
+//if marca un bloque de instrucciones que se ejecutan si una condicion se cumple 
+//swich marca un bloque de instrucciones que se ejcutan si una condicon se cumple
+//While marca un bloque de instrucciones que se ejecutan varias veces 
+//function marca un bloque de instrucciones que se ejcutan varias veces si se declaran 
+// try se ejecuta cuando se produce un error
+//catch se ejecuta cuando se produce un error
+//Return se ejecuta cuando una condicion se cumple ademas de devolver un valor 
